f440a520c6734c081ab2e2bd0116e3a98442c41f
getsalman <mohammad.salman@algorisys.com>
2020-08-24 19:30:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,118 +1,118 @@
-import React, { useReducer, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useImmerReducer } from "use-immer";
-import { CSSTransition } from "react-transition-group";
-import "./App.css";
+import React, { useReducer, useEffect } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { useImmerReducer } from 'use-immer'
+import { CSSTransition } from 'react-transition-group'
+import './App.css'
+import Axios from 'axios'
+import Header from './components/Header'
+import HomeGuest from './components/HomeGuest'
+import Home from './components/Home'
+import Footer from './components/Footer'
+import About from './components/About'
+import Terms from './components/Terms'
+import CreatePost from './components/CreatePost'
+import ViewSinglePost from './components/ViewSinglePost'
+import FlashMessages from './components/FlashMessages'
+import StateContext from './StateContext'
+import DispatchContext from './DispatchContext'
+import Profile from './components/Profile'
+import EditPost from './components/EditPost'
+import NotFound from './components/NotFound'
+import Search from './components/Search'
+import Chat from './components/Chat'
 
-import Axios from "axios";
-import Header from "./components/Header";
-import HomeGuest from "./components/HomeGuest";
-import Home from "./components/Home";
-import Footer from "./components/Footer";
-import About from "./components/About";
-import Terms from "./components/Terms";
-import CreatePost from "./components/CreatePost";
-import ViewSinglePost from "./components/ViewSinglePost";
-import FlashMessages from "./components/FlashMessages";
-import StateContext from "./StateContext";
-import DispatchContext from "./DispatchContext";
-import Profile from "./components/Profile";
-import EditPost from "./components/EditPost";
-import NotFound from "./components/NotFound";
-import Search from "./components/Search";
-import Chat from "./components/Chat";
-
-Axios.defaults.baseURL = "http://localhost:8080";
+Axios.defaults.baseURL =
+  process.env.BACKENDURL || 'https://getsocialpost.herokuapp.com'
 
 const App = () => {
   const initialState = {
-    loggedIn: Boolean(localStorage.getItem("socialappToken")),
+    loggedIn: Boolean(localStorage.getItem('socialappToken')),
     flashMessages: [],
     user: {
-      token: localStorage.getItem("socialappToken"),
-      username: localStorage.getItem("socialappUsername"),
-      avatar: localStorage.getItem("socialappAvatar"),
+      token: localStorage.getItem('socialappToken'),
+      username: localStorage.getItem('socialappUsername'),
+      avatar: localStorage.getItem('socialappAvatar')
     },
     isSearchOpen: false,
     isChatOpen: false,
-    unreadChatCount: 0,
-  };
+    unreadChatCount: 0
+  }
 
   const OurReducer = (draft, action) => {
     switch (action.type) {
-      case "login":
-        draft.loggedIn = true;
-        draft.user = action.data;
-        return;
-      case "logout":
-        draft.loggedIn = false;
-        return;
-      case "flashMessage":
-        draft.flashMessages.push(action.value);
-        return;
-      case "openSearch":
-        draft.isSearchOpen = true;
-        return;
-      case "closeSearch":
-        draft.isSearchOpen = false;
-        return;
-      case "toggleChat":
-        draft.isChatOpen = !draft.isChatOpen;
-        return;
-      case "closeChat":
-        draft.isChatOpen = false;
-        return;
-      case "incrementUnreadChatCount":
-        draft.unreadChatCount++;
-        return;
-      case "clearUnreadChatCount":
-        draft.unreadChatCount = 0;
-        return;
+      case 'login':
+        draft.loggedIn = true
+        draft.user = action.data
+        return
+      case 'logout':
+        draft.loggedIn = false
+        return
+      case 'flashMessage':
+        draft.flashMessages.push(action.value)
+        return
+      case 'openSearch':
+        draft.isSearchOpen = true
+        return
+      case 'closeSearch':
+        draft.isSearchOpen = false
+        return
+      case 'toggleChat':
+        draft.isChatOpen = !draft.isChatOpen
+        return
+      case 'closeChat':
+        draft.isChatOpen = false
+        return
+      case 'incrementUnreadChatCount':
+        draft.unreadChatCount++
+        return
+      case 'clearUnreadChatCount':
+        draft.unreadChatCount = 0
+        return
     }
-  };
+  }
 
-  const [state, dispatch] = useImmerReducer(OurReducer, initialState);
+  const [state, dispatch] = useImmerReducer(OurReducer, initialState)
 
   useEffect(() => {
     if (state.loggedIn) {
-      localStorage.setItem("socialappToken", state.user.token); //the first argument is the name of data that we want to store, and the second argument is the value
-      localStorage.setItem("socialappUsername", state.user.username);
-      localStorage.setItem("socialappAvatar", state.user.avatar);
+      localStorage.setItem('socialappToken', state.user.token) //the first argument is the name of data that we want to store, and the second argument is the value
+      localStorage.setItem('socialappUsername', state.user.username)
+      localStorage.setItem('socialappAvatar', state.user.avatar)
     } else {
-      localStorage.removeItem("socialappToken"); //the first argument is the name of data that we want to store, and the second argument is the value
-      localStorage.removeItem("socialappUsername");
-      localStorage.removeItem("socialappAvatar");
+      localStorage.removeItem('socialappToken') //the first argument is the name of data that we want to store, and the second argument is the value
+      localStorage.removeItem('socialappUsername')
+      localStorage.removeItem('socialappAvatar')
     }
-  }, [state.loggedIn]);
+  }, [state.loggedIn])
 
   //Check if token has expired or not on first render
 
   useEffect(() => {
     if (state.loggedIn) {
-      const ourRequest = Axios.CancelToken.source();
-      async function fetchResult() {
+      const ourRequest = Axios.CancelToken.source()
+      async function fetchResult () {
         try {
           const response = await Axios.post(
-            "/checkToken",
+            '/checkToken',
             { token: state.user.token },
             { cancelToken: ourRequest.token }
-          );
+          )
           if (!response.data) {
-            dispatch({ type: "logout" });
+            dispatch({ type: 'logout' })
             dispatch({
-              type: "flashMessage",
-              value: "Your session has expired. Please login again.",
-            });
+              type: 'flashMessage',
+              value: 'Your session has expired. Please login again.'
+            })
           }
-          console.log(response.data);
+          console.log(response.data)
         } catch (error) {
-          console.log("There was a problem");
+          console.log('There was a problem')
         }
       }
-      fetchResult();
-      return () => ourRequest.cancel();
+      fetchResult()
+      return () => ourRequest.cancel()
     }
-  }, []);
+  }, [])
 
   return (
     <StateContext.Provider value={state}>
@@ -121,23 +121,23 @@ const App = () => {
           <FlashMessages messages={state.flashMessages} />
           <Header />
           <Switch>
-            <Route path="/profile/:username">
+            <Route path='/profile/:username'>
               <Profile />
             </Route>
-            <Route exact path="/">
+            <Route exact path='/'>
               {state.loggedIn ? <Home /> : <HomeGuest />}
             </Route>
-            <Route exact path="/post/:id">
+            <Route exact path='/post/:id'>
               <ViewSinglePost />
             </Route>
-            <Route exact path="/post/:id/edit">
+            <Route exact path='/post/:id/edit'>
               <EditPost />
             </Route>
-            <Route exact path="/create-post">
+            <Route exact path='/create-post'>
               <CreatePost />
             </Route>
-            <Route exact path="/about-us" component={About} />
-            <Route exact path="/terms" component={Terms} />
+            <Route exact path='/about-us' component={About} />
+            <Route exact path='/terms' component={Terms} />
             <Route>
               <NotFound />
             </Route>
@@ -145,7 +145,7 @@ const App = () => {
           <CSSTransition
             timeout={330}
             in={state.isSearchOpen}
-            classNames="search-overlay"
+            classNames='search-overlay'
             unmountOnExit
           >
             <Search />
@@ -155,7 +155,7 @@ const App = () => {
         </Router>
       </DispatchContext.Provider>
     </StateContext.Provider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,108 +1,110 @@
-import React, { useEffect, useContext, useRef } from "react";
-import { useImmer } from "use-immer";
-import io from "socket.io-client";
-import { Link } from "react-router-dom";
-import StateContext from "../StateContext";
-import DispatchContext from "../DispatchContext";
+import React, { useEffect, useContext, useRef } from 'react'
+import { useImmer } from 'use-immer'
+import io from 'socket.io-client'
+import { Link } from 'react-router-dom'
+import StateContext from '../StateContext'
+import DispatchContext from '../DispatchContext'
 
-const socket = io("http://localhost:8080");
+const socket = io(
+  process.env.BACKENDURL || 'https://getsocialpost.herokuapp.com'
+)
 
 const Chat = () => {
-  const chatField = useRef(null);
-  const chatLog = useRef(null);
-  const appState = useContext(StateContext);
-  const appDispatch = useContext(DispatchContext);
+  const chatField = useRef(null)
+  const chatLog = useRef(null)
+  const appState = useContext(StateContext)
+  const appDispatch = useContext(DispatchContext)
   const [state, setState] = useImmer({
-    fieldValue: "",
-    chatMessages: [],
-  });
+    fieldValue: '',
+    chatMessages: []
+  })
 
   useEffect(() => {
     if (appState.isChatOpen) {
-      chatField.current.focus();
-      appDispatch({ type: "clearUnreadChatCount" });
+      chatField.current.focus()
+      appDispatch({ type: 'clearUnreadChatCount' })
     }
-  }, [appState.isChatOpen]);
+  }, [appState.isChatOpen])
 
   useEffect(() => {
-    socket.on("chatFromServer", (message) => {
-      setState((draft) => {
-        draft.chatMessages.push(message);
-      });
-    });
-  }, []);
+    socket.on('chatFromServer', message => {
+      setState(draft => {
+        draft.chatMessages.push(message)
+      })
+    })
+  }, [])
 
   useEffect(() => {
-    chatLog.current.scrollTop = chatLog.current.scrollHeight;
+    chatLog.current.scrollTop = chatLog.current.scrollHeight
 
     if (state.chatMessages.length && !appState.isChatOpen) {
-      appDispatch({ type: "incrementUnreadChatCount" });
+      appDispatch({ type: 'incrementUnreadChatCount' })
     }
-  }, [state.chatMessages]);
+  }, [state.chatMessages])
 
-  const handleFieldChange = (e) => {
-    const value = e.target.value;
-    setState((draft) => {
-      draft.fieldValue = value;
-    });
-  };
+  const handleFieldChange = e => {
+    const value = e.target.value
+    setState(draft => {
+      draft.fieldValue = value
+    })
+  }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = e => {
+    e.preventDefault()
     // Send Message to chat server
 
-    socket.emit("chatFromBrowser", {
+    socket.emit('chatFromBrowser', {
       message: state.fieldValue,
-      token: appState.user.token,
-    });
-    setState((draft) => {
+      token: appState.user.token
+    })
+    setState(draft => {
       // Add message to state collection of messages
       draft.chatMessages.push({
         message: draft.fieldValue,
         username: appState.user.username,
-        avatar: appState.user.avatar,
-      });
-      draft.fieldValue = "";
-    });
-  };
+        avatar: appState.user.avatar
+      })
+      draft.fieldValue = ''
+    })
+  }
 
   return (
     <div
-      id="chat-wrapper"
+      id='chat-wrapper'
       className={
-        "chat-wrapper shadow border-top border-left border-right " +
-        (appState.isChatOpen ? "chat-wrapper--is-visible" : "")
+        'chat-wrapper shadow border-top border-left border-right ' +
+        (appState.isChatOpen ? 'chat-wrapper--is-visible' : '')
       }
     >
-      <div className="chat-title-bar bg-primary">
+      <div className='chat-title-bar bg-primary'>
         Chat
         <span
-          onClick={() => appDispatch({ type: "closeChat" })}
-          className="chat-title-bar-close"
+          onClick={() => appDispatch({ type: 'closeChat' })}
+          className='chat-title-bar-close'
         >
-          <i className="fas fa-times-circle"></i>
+          <i className='fas fa-times-circle'></i>
         </span>
       </div>
-      <div id="chat" className="chat-log" ref={chatLog}>
+      <div id='chat' className='chat-log' ref={chatLog}>
         {state.chatMessages.map((message, index) => {
           if (message.username == appState.user.username) {
             return (
-              <div key={index} className="chat-self">
-                <div className="chat-message">
-                  <div className="chat-message-inner">{message.message}</div>
+              <div key={index} className='chat-self'>
+                <div className='chat-message'>
+                  <div className='chat-message-inner'>{message.message}</div>
                 </div>
-                <img className="chat-avatar avatar-tiny" src={message.avatar} />
+                <img className='chat-avatar avatar-tiny' src={message.avatar} />
               </div>
-            );
+            )
           }
 
           return (
-            <div key={index} className="chat-other">
+            <div key={index} className='chat-other'>
               <Link to={`/profile/${message.username}`}>
-                <img className="avatar-tiny" src={message.avatar} />
+                <img className='avatar-tiny' src={message.avatar} />
               </Link>
-              <div className="chat-message">
-                <div className="chat-message-inner">
+              <div className='chat-message'>
+                <div className='chat-message-inner'>
                   <Link to={`/profile/${message.username}`}>
                     <strong>{message.username}: </strong>
                   </Link>
@@ -110,27 +112,27 @@ const Chat = () => {
                 </div>
               </div>
             </div>
-          );
+          )
         })}
       </div>
       <form
         onSubmit={handleSubmit}
-        id="chatForm"
-        className="chat-form border-top"
+        id='chatForm'
+        className='chat-form border-top'
       >
         <input
           value={state.fieldValue}
           onChange={handleFieldChange}
           ref={chatField}
-          type="text"
-          className="chat-field"
-          id="chatField"
-          placeholder="Type a message…"
-          autoComplete="off"
+          type='text'
+          className='chat-field'
+          id='chatField'
+          placeholder='Type a message…'
+          autoComplete='off'
         />
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default Chat;
+export default Chat
